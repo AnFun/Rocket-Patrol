c3e34c31ba58652f81c31dfd43c8f6c0840a8ed1
AnFun <qsatow@gmail.com>
2021-04-12 20:10:46 -0700

    final edits

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ let config = {
     height: 480,
     scene: [Menu, Play]
 }
+
 let game = new Phaser.Game(config);
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
